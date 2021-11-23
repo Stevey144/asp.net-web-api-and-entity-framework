e70daadac64ff3892fe3602f400318455d0c363e
Stevey144 <stephenogechi455@gmail.com>
2021-11-23 17:50:21 +0100

    asp.net web api and entity framework

---
+++ EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeService.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<EMPLOYEE> Get()
+        {
+            using (STEPHENEntities entities = new STEPHENEntities())
+            {
+                return entities.EMPLOYEES.ToList();
+            }
+        }
+
+        public EMPLOYEE Get(int id)
+        {
+            using (STEPHENEntities entities = new STEPHENEntities())
+            {
+                return entities.EMPLOYEES.FirstOrDefault(e => e.ID == id);
+            }
+        }
+
+    }
+}
