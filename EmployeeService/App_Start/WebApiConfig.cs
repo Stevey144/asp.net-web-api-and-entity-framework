088e30a7224f070ffcbfedd816fed64582bcb962
Stevey144 <stephenogechi455@gmail.com>
2021-12-03 14:46:33 +0100

    added functionality to call web api with jquery

--- EmployeeService/App_Start/WebApiConfig.cs
+++ EmployeeService/App_Start/WebApiConfig.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 
 namespace EmployeeService
 {
+    //public class CustomJsonFormater : JsonMediaTypeFormatter
+    //{
+    //    public CustomJsonFormater()
+    //    {
+    //        this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+    //    }
+
+    //    public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+    //    {
+    //        base.SetDefaultContentHeaders(type, headers, mediaType);
+    //        headers.ContentType = new MediaTypeHeaderValue("application/json");
+    //    }
+
+    //}
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            //config.SuppressHostPrincipal();
             // Web API routes
             config.MapHttpAttributeRoutes();
 
@@ -19,6 +36,18 @@ namespace EmployeeService
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            //config.Formatters.Remove(config.Formatters.JsonFormatter);// use this if u want to create a service that supports only
+            // json formatter not xml, because this removes the xml
+            //do the opposite if you want to get only xml response
+
+            //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html")); //
+
+
+            //config.Formatters.Add(new CustomJsonFormater());
+
+            //config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;// use this to arrange the json
+
+
         }
     }
 }
--- EmployeeService/Controllers/EmployeesController.cs
+++ EmployeeService/Controllers/EmployeesController.cs
@@ -9,21 +9,128 @@ namespace EmployeeService.Controllers
 {
     public class EmployeesController : ApiController
     {
-        public IEnumerable<EMPLOYEE> Get()
+        //public IEnumerable<EMPLOYEE> Get()
+        //{
+        //    using (STEPHENEntities entities = new STEPHENEntities())
+        //    {
+        //        return entities.EMPLOYEES.ToList();
+        //    }
+        //}
+
+
+        public HttpResponseMessage Get(string gender = "All")
         {
             using (STEPHENEntities entities = new STEPHENEntities())
             {
-                return entities.EMPLOYEES.ToList();
+                switch (gender.ToLower())
+                {
+                    case "all":
+                        return Request.CreateResponse(HttpStatusCode.OK, entities.EMPLOYEES.ToList());
+                    case "male":
+                        return Request.CreateResponse(HttpStatusCode.OK, entities.EMPLOYEES.Where(e => e.GENDER.ToLower() == "male").ToList());
+                    case "female":
+                        return Request.CreateResponse(HttpStatusCode.OK, entities.EMPLOYEES.Where(e => e.GENDER.ToLower() == "female").ToList());
+
+                    default:
+                        return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "value for gender must be male or female. " + gender + " is invalid");
+                }
+
             }
         }
 
-        public EMPLOYEE Get(int id)
+        public HttpResponseMessage Get(int id)
         {
             using (STEPHENEntities entities = new STEPHENEntities())
             {
-                return entities.EMPLOYEES.FirstOrDefault(e => e.ID == id);
+                var entity = entities.EMPLOYEES.FirstOrDefault(e => e.ID == id);
+                if (entity != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "employee with id =" + id.ToString() + " not found");
+                }
             }
         }
 
+        public HttpResponseMessage post([FromBody] EMPLOYEE employee)
+        {
+            try
+            {
+                using (STEPHENEntities entities = new STEPHENEntities())
+                {
+                    entities.EMPLOYEES.Add(employee);
+                    entities.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    message.Headers.Location = new Uri(Request.RequestUri + employee.ID.ToString());
+                    return message;
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, e);
+            }
+        }
+
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                using (STEPHENEntities entities = new STEPHENEntities())
+                {
+                    var entity = entities.EMPLOYEES.FirstOrDefault(e => e.ID == id);
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with id =" + id.ToString() + "not found to delete");
+                    }
+                    else
+                    {
+                        entities.EMPLOYEES.Remove(entity);
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK);
+
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+
+        }
+        public HttpResponseMessage put(int id, [FromBody] EMPLOYEE employee)
+        {
+            try {
+            using (STEPHENEntities entities = new STEPHENEntities())
+            {
+
+                var entity = entities.EMPLOYEES.FirstOrDefault(e => e.ID == id);
+                if (entity == null)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "employee with id = " + id.ToString() + " not found");
+                }
+                else
+                {
+                    entity.FIRSTNAME = employee.FIRSTNAME;
+                    entity.LASTNAME = employee.LASTNAME;
+                    entity.GENDER = employee.GENDER;
+                    entity.SALARY = employee.SALARY;
+
+                    entities.SaveChanges(); 
+                    return Request.CreateResponse(HttpStatusCode.OK,entity);
+                }
+
+            }
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+
+        }
     }
 }
